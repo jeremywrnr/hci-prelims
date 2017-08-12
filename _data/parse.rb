2ad2fbf82627c17c459ba7c97a116a4bc121f3f7
Jeremy Warner <jeremywrnr@gmail.com>
2017-08-12 17:55:01 -0400

    better printing

--- _data/parse.rb
+++ _data/parse.rb
@@ -7,9 +7,9 @@ data = YAML.load(File.read("./papers.yaml"))
 # TODO - some merging based on features to generate all the other lists of
 # papers, or otherwise see if they can just be made from jekyll sorting
 
-puts data.to_json
-
 # you could edit the data here to assign ids to each paper which could then be
 # used in the paper graph that sarah made.
-
 #JSON.parse(data.to_s)
+
+# print it out
+puts JSON.pretty_generate(JSON.parse(data.to_json.to_s))
